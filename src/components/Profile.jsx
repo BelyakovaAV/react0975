518fb948a2d4ffa345a0271d3f51e7bf931ab78b
BelyakovaAV <avbelyakovakm@gmail.com>
2022-12-14 00:03:31 +0300

    add style for Profile, add state

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,3 +1,5 @@
+import styles from "./Profile.module.css";
+
 const Profile = ()=> {
   return (
     <div className="row">
@@ -5,11 +7,12 @@ const Profile = ()=> {
         <img src="https://www.seekpng.com/png/full/356-3562377_personal-user.png" alt="" width={"100%np"} />
       </div>
       <div className="col-sm-9">
-        <h1>Фамилия и Имя</h1>
-        <h2>Рассказ о себе</h2>
+        <h1 style={{backgroundColor: "lightblue"}}>Фамилия и Имя</h1>
+        <h2 className={styles.about}>Рассказ о себе</h2>
         <p>E-mail: ivan@ya.ru</p>
         <p>Id: 2</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni eveniet quibusdam vitae nesciunt ab, sit tenetur soluta earum delectus nihil voluptate commodi quos, odit hic eligendi officia corporis eius? Dolor!</p>
+        <p className={styles.paragraph}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni eveniet quibusdam vitae nesciunt ab, sit tenetur soluta earum delectus nihil voluptate commodi quos, odit hic eligendi officia corporis eius? Dolor!</p>
       </div>
     </div>
   );
